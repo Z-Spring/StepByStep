1880d6c34c7d870d45f4681805778b04e5b6dfe7
murphy <murphyqq1@gmail.com>
2023-12-11 21:39:32 +0800

    feature: add floor pool

---
+++ Assets/Scripts/Pool/FloorPool.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pool
+{
+    public class FloorPool : MonoBehaviour
+    {
+        public static FloorPool Instance { get; private set; }
+
+        [SerializeField] int initFloorNum;
+        [SerializeField] GameObject floorPrefab;
+
+        readonly Queue<GameObject> floorPool = new();
+
+        private void Awake()
+        {
+            Instance = this;
+            InitPool();
+        }
+
+        void InitPool()
+        {
+            SpawnFloor(initFloorNum);
+        }
+
+        void SpawnFloor(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                GameObject go = Instantiate(floorPrefab,transform);
+                go.SetActive(false);
+                floorPool.Enqueue(go);
+            }
+        }
+
+        public void ReturnFloor(GameObject floor)
+        {
+            floorPool.Enqueue(floor);
+            floor.SetActive(false);
+        }
+
+        public GameObject GetFloorFromPool()
+        {
+            if (floorPool.Count == 0)
+            {
+                SpawnFloor(1);
+            }
+
+            GameObject go = floorPool.Dequeue();
+            go.SetActive(true);
+            return go;
+        }
+    }
+}
